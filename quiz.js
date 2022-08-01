a4804062ea2c922b66d0a4efed5a987cbfe05d3e
Pegrume88 <pegrume88@gmail.com>
2022-08-01 08:56:45 +0000

    created variables for current question, accepting the correct answer, keeping score,question counter and a variable array to choose the question

---
+++ quiz.js
@@ -0,0 +1,9 @@
+const question = document.getElementById('question');
+const answers = Array.from(document.getElementsByClassName("answer-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+
+
+
+
