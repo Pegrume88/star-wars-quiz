c20aa354cfc62693cf05e2c40ece59b4a672bdd4
Pegrume88 <pegrume88@gmail.com>
2022-09-13 12:19:40 +0000

    added mor customized code to show understanding commented out more code for same reason

--- quiz.js
+++ quiz.js
@@ -1,121 +1,125 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const questionCounterText = document.getElementById('questionCounter');
-const ScoreText = document.getElementById('score')
 
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
-
-/* Array of all the questions and answers, with the correct answer*/ 
+/* All the questions and answers, with the correct answer*/ 
 
 let questions = [
     {
         question: "What is Baby Yoda's real name?",
-        choice1: "Goku",
-        choice2: "Grogu",
-        choice3: "Boda",
-        choice4: "Yogi",
+        option1: "Goku",
+        option2: "Grogu",
+        option3: "Boda",
+        option4: "Yogi",
         answer: 2
     },
     {
         question: "Where did Obi-Wan take Luke after his birth?",
-        choice1: "Hoth",
-        choice2: "Naboo",
-        choice3: 'Tatooine',
-        choice4: "Kamino",
+        option1: "Hoth",
+        option2: "Naboo",
+        option3: 'Tatooine',
+        option4: "Kamino",
         answer: 3
     },
     {
         question: "Who was Anakin Skywalker's Padawan",
-        choice1: "Ahsoka Tano",
-        choice2: "Qui-Gon Jinn",
-        choice3: "Kanan Jarrus",
-        choice4: "Mace Windu",
+        option1: "Ahsoka Tano",
+        option2: "Qui-Gon Jinn",
+        option3: "Kanan Jarrus",
+        option4: "Mace Windu",
         answer: 1
     },
     {
         question: "What year did the first Star Wars movie come out?",
-        choice1: '1977',
-        choice2: '1975',
-        choice3: '1991',
-        choice4: '1981',
+        option1: '1977',
+        option2: '1975',
+        option3: '1991',
+        option4: '1981',
         answer: 1
     },
     {
         question: "Who had the highest midi-chlorian count in Star Wars?",
-        choice1: "Luke Skywalker",
-        choice2: "Obi-Wan Kenobi",
-        choice3: "Count Dooku",
-        choice4: "Anakin Skywalker",
+        option1: "Luke Skywalker",
+        option2: "Obi-Wan Kenobi",
+        option3: "Count Dooku",
+        option4: "Anakin Skywalker",
         answer: 4
     },
     {
         question: "Who killed Qui-Gon Jinn?",
-        choice1: "Darth Vader",
-        choice2: "Palpatine",
-        choice3: "Darth Maul",
-        choice4: "Han Solo",
+        option1: "Darth Vader",
+        option2: "Palpatine",
+        option3: "Darth Maul",
+        option4: "Han Solo",
         answer: 3
     },
     {
         question: "What is the name of Han Solo's ship?",
-        choice1: "The Millennium Falcon",
-        choice2: "Enterprise",
-        choice3: "Death Star",
-        choice4: "Apollo",
+        option1: "The Millennium Falcon",
+        option2: "Enterprise",
+        option3: "Death Star",
+        option4: "Apollo",
         answer: 1
     },
     {
         question: "Where was Yoda's home in his final years?",
-        choice1: 'Middle Earth',
-        choice2: "Dagobah",
-        choice3: "Bespin",
-        choice4: "Crait",
+        option1: 'Middle Earth',
+        option2: "Dagobah",
+        option3: "Bespin",
+        option4: "Crait",
         answer: 2
     },
     {
         question: "Han Solo was frozen in what?",
-        choice1: "Crystal",
-        choice2: "Ice",
-        choice3: "Time",
-        choice4: "Carbonite",
+        option1: "Crystal",
+        option2: "Ice",
+        option3: "Time",
+        option4: "Carbonite",
         answer: 4
     },
     {
         question: "Who killed Jabba?",
-        choice1: "Han Solo",
-        choice2: "Luke Skywalker",
-        choice3: "Princess Leia",
-        choice4: "Boba Fett",
+        option1: "Han Solo",
+        option2: "Luke Skywalker",
+        option3: "Princess Leia",
+        option4: "Boba Fett",
         answer: 3
     },
     {
         question: "What species is Chewbacca?",
-        choice1: "wookie",
-        choice2: "Cookie",
-        choice3: "Ewok",
-        choice4: "Zabrak",
+        option1: "wookie",
+        option2: "Cookie",
+        option3: "Ewok",
+        option4: "Zabrak",
         answer: 1
     },
     {
         question: "Padmé was Queen of what?",
-        choice1: "The Hutts",
-        choice2: "The Empire",
-        choice3: "England",
-        choice4: "Naboo",
+        option1: "The Hutts",
+        option2: "The Empire",
+        option3: "England",
+        option4: "Naboo",
         answer: 4
     }
-   
+
 ]
 
 /**
- * Constants for scoring 10 points for correct answer and the max amount of questions in this quiz
+ * scoring 10 points for correct answer + display score
  */
-const SCORE_POINT = 10;
-const MAX_QUESTIONS = 12;
+const POINTS = 10;
+
+const ScoreText = document.getElementById('score')
+let score = 0;
+
+const TOTAL_QUESTIONS = 12;
+const question = document.querySelector('#question');
+const questionCounterText = document.getElementById('questionCounter');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+
+let questionCounter = 0;
+let availableQuestions = [];
+
 /** start game function with score,question counter, copied array of question and get new question function */
 startGame = () =>
 {
@@ -125,28 +129,34 @@ startGame = () =>
     getNewQuestions();
 };
 
+//show incremented score
+incrementScore = num => {
+    score += num;
+    ScoreText.innerText = score;
+}
+
 /** function for selecting new random question and displaying question text */
+
 
 getNewQuestions = () => {
 
-    
-
-    if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+    if (availableQuestions.length == 0 || questionCounter >= TOTAL_QUESTIONS){
         localStorage.setItem("mostRecentScore", score);
 
 //when max questions are reached send player to end page
         return window.location.assign("end.html");
     }
   questionCounter++ ;
-  questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
-  
+  questionCounterText.innerText = questionCounter + "/" + TOTAL_QUESTIONS;
+
+//random question selection  
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
 
-    choices.forEach( choice => {
-        const number = choice.dataset ["number"];
-        choice.innerText = currentQuestion["choice" + number ];
+    choices.forEach( option => {
+        const number = option.dataset ["number"];
+        option.innerText = currentQuestion["option" + number ];
     })
     /** removing answered questions */
     availableQuestions.splice(questionIndex, 1);
@@ -154,8 +164,8 @@ getNewQuestions = () => {
     acceptingAnswers = true;
 };
 
-choices.forEach( choice => {
-    choice.addEventListener("click", e => {
+choices.forEach( option => {
+    option.addEventListener("click", e => {
         if (!acceptingAnswers) return;
         acceptingAnswers = false;
         const selectedChoice = e.target;
@@ -165,16 +175,16 @@ choices.forEach( choice => {
         const classToApply = selectedAnswer == currentQuestion.answer ? "correct"
 : "incorrect";
         
-// increment score by to for correct answer
+// increment score by 10 for correct answer
         if(classToApply === "correct") {
-            incrementScore(SCORE_POINT);
+            incrementScore(POINTS);
         }
 
         selectedChoice.parentElement.classList.add(classToApply);
 
-        //time out added to allow time before removing class
+        //time delay before moving to next question
 
-        setTimeout (() => {
+            setTimeout (() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestions();
         }, 1000); 
@@ -182,9 +192,6 @@ choices.forEach( choice => {
     });
 });
 
-incrementScore = num => {
-    score += num;
-    ScoreText.innerText = score;
-}
+
 
 startGame();
